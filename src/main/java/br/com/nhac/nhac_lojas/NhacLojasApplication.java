9363b7644bc36fc5285da95b25805d4451e172e4
= <nikollasdheyvis17@gmail.com>
2026-08-14 10:26:17 -0300

    subindo aplicação spring

---
+++ src/main/java/br/com/nhac/nhac_lojas/NhacLojasApplication.java
@@ -0,0 +1,13 @@
+package br.com.nhac.nhac_lojas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhacLojasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhacLojasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nhac/nhac_lojas/NhacLojasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nhac.nhac_lojas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhacLojasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
